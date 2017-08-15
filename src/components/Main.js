4e3ba730e84aca3a7c388170f5c0c6024e8e935f
00ming <420738030@qq.com>
2017-08-15 09:20:33 +0800

    stage
    stage

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ import React from 'react';
 let yeomanImage = require('../images/yeoman.png');
 let json = require("../data/datas.json");
 // let json = require("./2.json");
-
+console.log(json)
 var imagesdatas = (function getImagesURL (arr) {
 	for (let i = 0;i < arr.length; i ++){
 		var singleImage = arr[i];
@@ -19,9 +19,9 @@ console.log(json[1])
 class AppComponent extends React.Component {
   render() {
     return (
-    	<section>
-    	<section></section>
-    	<nav></nav>
+    	<section className="stage">
+    	<section className="image-sec"></section>
+    	<nav className="controller-nav"></nav>
         </section>
     );
   }
