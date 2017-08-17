006e8a85fb23b005f70cdec45cc63a688cc06610
00ming <420738030@qq.com>
2017-08-17 17:11:49 +0800

    上侧图片
    上侧图片

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,8 +5,8 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 let yeomanImage = require('../images/yeoman.png');
 let json = require("../data/datas.json");
-// let json = require("./2.json");
-// console.log(json)
+
+
 var imagesdatas = (function getImagesURL (arr) {
 	for (let i = 0;i < arr.length; i ++){
 		var singleImage = arr[i];
@@ -19,15 +19,29 @@ var imagesdatas = (function getImagesURL (arr) {
 
 class ImgFigure extends React.Component {
   render () {
+
+    var styleObj = {};
+
+    // 如果props属性中制定了这张图片的位置，则使用
+    if (this.props.arrange) {
+      styleObj = this.props.arrange.pos;
+    }
+    // console.log(this.props.data)
+    if(this.props.data.fileName === '1.jpg') {
+
+
+    }
+    
+
+
     return (
-      <figure className="img-figure">
+      <figure className="img-figure" style={styleObj}>
         <img src={this.props.data.imageURL} alt={this.props.data.title}/>
         <figcaption>
           <h2 className="img-title">{this.props.data.title}</h2>
         </figcaption>
       </figure>
     )
-
   }
 }
 // 获取区间内的随机值
@@ -52,12 +66,12 @@ class AppComponent extends React.Component {
          topY: [0,0]
        },
        imgsArrangeArr: [
-         {
-            // pos: {
-            //   left: '0',
-            //   top: '0'
-            // }
-         }
+         // {
+         //    pos: {
+         //      left: '0',
+         //      top: '0'
+         //    }
+         // }
         ]
 
     };
@@ -79,16 +93,21 @@ class AppComponent extends React.Component {
           imgsArrangeTopArr = [],//存放上方图片的信息
           topImgNum = Math.ceil(Math.random() * 2), //取一个或者不取
           topImgSpliceIndex = 0,
-          imgsArrageCenterArr = imgsArrangeArr.splice(centerIndex,1);//中心图片信息
           
-
-          // console.log(this.state)
+          imgsArrageCenterArr = imgsArrangeArr.splice(centerIndex,1);//中心图片信息
+     
           // 先居中
           imgsArrageCenterArr[0].pos = centerPos;
-          // 上侧状态信息
-          topImgSpliceIndex = Math.ceil(Math.random()*(imgsArrangeArr.length - topImgNum))
-          imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex,topImgNum);
 
+
+
+          // 上侧状态信息
+          topImgSpliceIndex = Math.ceil(Math.random()*(imgsArrangeArr.length - topImgNum -1))
+          
+          imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex,topImgNum - 1);
+          console.log("上侧属性")
+          console.log(imgsArrangeTopArr)
+          
           // 布局上侧的图片
           imgsArrangeTopArr.forEach(function (value,index) {
             imgsArrangeTopArr[index].pos = {
@@ -97,6 +116,31 @@ class AppComponent extends React.Component {
             }
           })
           // 布局左右侧图片
+          for (var i = 0,j = imgsArrangeArr.length,k = j / 2;i < j; i++) {
+            var hPosRangeLORX = null;
+
+            //一半在左一半在右
+            if (i < k) {
+              hPosRangeLORX = hPosRangeleftSecX;
+            } else {
+              hPosRangeLORX = hPosRangerightSecX;
+            }
+
+            imgsArrangeArr[i].pos = {
+              top: getRangeRandom(hposRangeY[0],hposRangeY[1]),
+              left: getRangeRandom(hPosRangeLORX[0],hPosRangeLORX[1])
+            }
+          }
+          if (imgsArrangeTopArr && imgsArrangeTopArr[0]) {
+            imgsArrangeArr.splice(topImgSpliceIndex,0,imgsArrangeTopArr[0]);
+          }
+          imgsArrangeArr.splice(centerIndex,0,imgsArrageCenterArr[0]);
+          
+          this.setState({
+            imgsArrangeArr: imgsArrangeArr
+          })
+          console.log(this.state.imgsArrangeArr)
+         
 
     };
 
@@ -126,17 +170,19 @@ class AppComponent extends React.Component {
         left: halfStageW - halfImgW,
         top: halfStageH - halfImgH
       }
+    
        // 计算左右侧区域图片位置的取值的范围
       this.state.hPosRange.leftSecX[0] = -halfImgW;
       this.state.hPosRange.leftSecX[1] = halfStageW - halfImgW * 3;
-      this.state.hPosRange.rightSecX[0] = halfStageW - halfImgW;
+      this.state.hPosRange.rightSecX[0] = halfStageW + halfImgW;
       this.state.hPosRange.rightSecX[1] = stageW - halfImgW;
       this.state.hPosRange.y[0] = -halfImgH;
       this.state.hPosRange.y[1] = stageH - halfImgH
       // 计算上侧区域图片位置的取值的范围
       this.state.vPosRange.topY[0] = -halfImgH;
       this.state.vPosRange.topY[1] = halfStageH - halfImgH * 3;
-      this.state.vPosRange.x[0] = halfImgW;
+      this.state.vPosRange.x[0] = halfStageW - imgW;
+      this.state.vPosRange.x[1] = halfImgW;
 
       
       this.rearrange(0)
@@ -145,18 +191,22 @@ class AppComponent extends React.Component {
   render() { 
      var controllerUnits = [],
         imgFigures = [];
-    // 下面这个foreach莫名其妙报错
-    imagesdatas.forEach(function (value,index) {
-      imgFigures.push(<ImgFigure data={value} key={index} ref={'imgFigure' + index}/>);
-      if ( !this.state.imgsArrangeArr[index]) {
-        this.state.imgsArrangeArr[index] = {
-          pos: {
-            left: 0,
-            top: 0
+     imagesdatas.forEach(function (value,index) {
+        imgFigures.push(<ImgFigure data={value} key={index} ref={'imgFigure' + index} arrange={this.state.imgsArrangeArr[index]} />);
+        
+
+        if ( !this.state.imgsArrangeArr[index]) {
+          this.state.imgsArrangeArr[index] = {
+            pos: {
+              left: 0,
+              top: 0
+            }
           }
         }
-      }
-    }.bind(this));
+        
+        
+      }.bind(this));
+      
     return (
     	<section className="stage" ref="stage">
     	  <section className="image-sec">
